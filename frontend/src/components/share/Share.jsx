83ba64ec2c2ee0ee9827023951ae67ea945d2162
Mai <111605397+mai0711@users.noreply.github.com>
2023-08-07 17:31:15 +0900

    finished 122

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -1,10 +1,10 @@
 //ホーム画面の投稿フォーム
 
-import React from 'react'
-import { useContext } from 'react';
+import React, { useContext, useRef } from 'react'
 import "./Share.css"
 import { Image, Gif, Face, Analytics } from "@mui/icons-material";
 import { AuthContext } from "../../state/AuthContext"
+import axios from 'axios';
 
 
 export default function Share() {
@@ -12,6 +12,22 @@ export default function Share() {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER
 
   const { user } = useContext(AuthContext);
+  const desc = useRef()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id, //誰が投稿するか（今ログインしているユーザー）
+      description: desc.current.value, //inputで入力した文字
+    };
+    //api
+    try {
+      await axios.post("/posts", newPost); //post.jsの1
+      window.location.reload(); //投稿した後、自分でリロードしなくても良くなる
+    } catch(err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className='share'>
@@ -25,10 +41,16 @@ export default function Share() {
           alt=''
           className='shareProfileImg'
           />
-          <input type='text' className='shareInput' placeholder='What are you doing now?' />
+          <input
+          type='text'
+          className='shareInput'
+          placeholder='What are you doing now?'
+          ref={desc}
+          />
         </div>
         <hr className="shareHr" />
-        <div className="shareButtons">
+
+        <form className="shareButtons" onSubmit={(e) => handleSubmit(e)}>
           <div className="shareOptions">
             <div className="shareOption">
               <Image className="shareIcon" htmlColor='blue'/>
@@ -47,8 +69,8 @@ export default function Share() {
               <span className="shareOptionText">Vote</span>
             </div>
           </div>
-          <button className='shareButton'>Post</button>
-        </div>
+          <button className='shareButton' type='submit'>Post</button>
+        </form>
       </div>
     </div>
   )
